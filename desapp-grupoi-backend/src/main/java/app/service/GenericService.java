e48705349a9384297bf96230f131f3eab53d42e5
Emmanuel Pericon <brianpericon.e92@gmail.com>
2019-05-03 00:23:55 -0300

    Agregado de rollback si falla transaccion

--- desapp-grupoi-backend/src/main/java/app/service/GenericService.java
+++ desapp-grupoi-backend/src/main/java/app/service/GenericService.java
@@ -2,6 +2,7 @@ package app.service;
 
 import app.persistence.GenericDao;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import utils.HibernateUtil;
 
@@ -19,13 +20,24 @@ public class GenericService<T> extends HibernateUtil {
     }
 
     public T getById(final Serializable id) {
-        Session session = this.getSessionFactory().openSession();
-        session.beginTransaction();
+        T t = null;
+        Session session = null;
+        Transaction tsn = null;
+        try{
+            session = this.getSessionFactory().openSession();
+            session.beginTransaction();
 
-        this.dao.setSession(session);
-        T t = dao.getById(id);
+            this.dao.setSession(session);
+            t = dao.getById(id);
 
-        session.getTransaction().commit();
-        return t;
+            session.getTransaction().commit();
+        }catch (Exception ex) {
+            ex.printStackTrace();
+            tsn.rollback();
+        }
+        finally {
+            session.close();
+            return t;
+        }
     }
 }
