18926882a1c6f14ad77d6396d0ffab7074c985b3
ale rossi <ale.h90@gmail.com>
2019-05-07 17:24:25 -0300

    cambio de librerias para front + arreglo de error en hibernate de memoria

--- desapp-grupoi-frontend/src/app/app.module.ts
+++ desapp-grupoi-frontend/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { LandingComponent } from './landing/landing.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
