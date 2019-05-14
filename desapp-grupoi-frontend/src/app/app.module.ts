b5654648b071dff8590edb37972b826fca577376
ale rossi <ale.h90@gmail.com>
2019-05-14 19:58:00 -0300

    inicio landing page en clase

--- desapp-grupoi-frontend/src/app/app.module.ts
+++ desapp-grupoi-frontend/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
+// import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatCardModule} from '@angular/material/card';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -15,8 +17,11 @@ import { LandingComponent } from './landing/landing.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule
   ],
+  exports: [MatButtonModule, MatCardModule],
   providers: [],
   bootstrap: [AppComponent]
 })
