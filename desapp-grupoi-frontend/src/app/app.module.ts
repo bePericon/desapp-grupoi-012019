ab3258420d09f4c66e039ff83bca413ccbdb90bc
ale rossi <ale.h90@gmail.com>
2019-05-18 18:53:51 -0300

    routing

--- desapp-grupoi-frontend/src/app/app.module.ts
+++ desapp-grupoi-frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {ErrorStateMatcher} from '@angular/material/core';
+import { ErrorStateMatcher } from '@angular/material/core';
 import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { MatCardModule } from '@angular/material/card';
 import { AppRoutingModule } from './app-routing.module';
@@ -11,7 +11,32 @@ import { MatInputModule } from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule, FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { EventosComponent } from './eventos/eventos.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: LandingComponent
+  } //,
+  // {
+  //   path: 'hero/:id',
+  //   component: HeroDetailComponent
+  // },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  // {
+  //   path: '',
+  //   redirectTo: '/heroes',
+  //   pathMatch: 'full'
+  // },
+  // {
+  //   path: '**',
+  //   component: PageNotFoundComponent
+  // }
+];
 
 @NgModule({
   declarations: [
@@ -22,6 +47,10 @@ import { EventosComponent } from './eventos/eventos.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserAnimationsModule,
     MatButtonModule,
     MatCardModule,
@@ -50,4 +79,4 @@ export class AppModule {
 
 
 
- }
+}
