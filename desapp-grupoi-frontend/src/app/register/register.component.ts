9a14ddbc2fc2f9df477dd59e437a56e2b85a040d
ale rossi <ale.h90@gmail.com>
2019-05-25 19:01:44 -0300

    register y otros arreglos

--- desapp-grupoi-frontend/src/app/register/register.component.ts
+++ desapp-grupoi-frontend/src/app/register/register.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { Router } from '@angular/router';
+// import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-register',
@@ -8,17 +17,39 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-  isLinear = false;
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
+
+  nombreForm: FormGroup;
+  apellidoForm: FormGroup;
+  emailForm: FormGroup;
+  contraseniaForm: FormGroup;
+  fechaNacForm: FormGroup;
+
+  matcher = new MyErrorStateMatcher();
+
   constructor(private _formBuilder: FormBuilder) { }
 
+
+  // emailFormControl = new FormControl('', [
+  //   Validators.required,
+  //   Validators.email
+  // ]);
+
+
   ngOnInit() {
-    this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required]
+    this.nombreForm = this._formBuilder.group({
+      nombreCtrl: ['', Validators.required]
     });
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required]
+    this.apellidoForm = this._formBuilder.group({
+      apellidoCtrl: ['', Validators.required]
+    });
+    this.emailForm = this._formBuilder.group({
+      emailCtrl: ['', [Validators.required, Validators.email]]
+    });
+    this.contraseniaForm = this._formBuilder.group({
+      contraseniaCtrl: ['', Validators.required]
+    });
+    this.fechaNacForm = this._formBuilder.group({
+      fechaNacCtrl: ['', Validators.required]
     });
   }
 
