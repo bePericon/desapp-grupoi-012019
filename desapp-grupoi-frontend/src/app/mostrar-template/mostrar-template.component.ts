4137aed11f45a7165d0312349202430edb0d0993
ale rossi <ale.h90@gmail.com>
2019-06-04 18:30:19 -0300

    cambios visuales chicos

--- desapp-grupoi-frontend/src/app/mostrar-template/mostrar-template.component.ts
+++ desapp-grupoi-frontend/src/app/mostrar-template/mostrar-template.component.ts
@@ -10,14 +10,18 @@ export class MostrarTemplateComponent implements OnInit {
   constructor() {
     //esto deberia venir desde un servicio
     this.templates = [
-    { nombre: "t1", descripcion: "sarasa" },
-    { nombre: "t2", descripcion: "sadsad" },
-    { nombre: "t3", descripcion: "nombre de template" },
-    { nombre: "t4", descripcion: "sasaadsadsadasrasa" },
-    { nombre: "t5", descripcion: "sarasasdsadsadsaa" }]
+    { nombre: "Template uno", descripcion: "Este es un template loco",modalidad:"Fiesta" },
+    { nombre: "Otro loco", descripcion: "Este es otro template loco",modalidad:"Canasta" },
+    { nombre: "Locochón", descripcion: "Template locochón",modalidad:"Baquita con recoleccion previa" },
+    { nombre: "Nombre de tempalte", descripcion: "un cuarto template",modalidad:"Fiesta" },
+    { nombre: "aaaa", descripcion: "sarlanga",modalidad:"Canasta" }]
   }
 
   ngOnInit() {
+  }
+
+  elegirTemplate(t){
+    console.log("Esto no le gusta al profe, pero: Felicitaciones, elegiste el template "+ t.nombre+ "que es "+ t.descripcion)
   }
 
 }
