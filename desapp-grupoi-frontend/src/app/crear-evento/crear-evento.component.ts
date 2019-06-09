953a0119f20c5ef35d4ae59011454f413f82a795
ale rossi <ale.h90@gmail.com>
2019-06-09 19:21:39 -0300

    comportamiento visual de invitar + eliminar invitado + fix en eventos

--- desapp-grupoi-frontend/src/app/crear-evento/crear-evento.component.ts
+++ desapp-grupoi-frontend/src/app/crear-evento/crear-evento.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormBuilder, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
-import {ErrorStateMatcher} from '@angular/material/core';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 export interface Combo {
   nombre: string;
   desc: string;
 }
-
-
 
 @Component({
   selector: 'app-crear-evento',
@@ -15,14 +13,13 @@ export interface Combo {
   styleUrls: ['./crear-evento.component.scss']
 })
 
-
 export class CrearEventoComponent implements OnInit {
   panelOpenState = false;
   // invitado:string;
   invitados;
-  
-  constructor(private _formBuilder: FormBuilder) { 
-    this.invitados=[];
+
+  constructor(private _formBuilder: FormBuilder) {
+    this.invitados = [];
   }
 
   ngOnInit() {
@@ -30,7 +27,7 @@ export class CrearEventoComponent implements OnInit {
     this.infoCanasta = "Descripcion de canasta";
     this.infoBaquitav1 = "Descripcion de baquita";
     this.infoBaquitav2 = "Descripcion de baquitav2";
-  
+
     this.modalidad = "";
 
     // this.modalidadControl = new FormControl('', [Validators.required]);
@@ -42,53 +39,60 @@ export class CrearEventoComponent implements OnInit {
     this.descripcionForm = this._formBuilder.group({
       descripcionCtrl: ['', Validators.required]
     });
-  
+
     this.fechaForm = this._formBuilder.group({
       fechaCtrl: ['', Validators.required]
     });
 
     this.emailForm = this._formBuilder.group({
-      emailCtrl: ['', [ Validators.email]]
+      emailCtrl: ['', [Validators.email]]
     });
   }
-  
-  modalidadControl:FormControl;
+
+  modalidadControl: FormControl;
   nombreForm: FormGroup;
   descripcionForm: FormGroup;
   fechaForm: FormGroup;
   emailForm: FormGroup;
 
-  infoFiesta:String;
-  infoCanasta:String;
-  infoBaquitav1:String;
-  infoBaquitav2:String;
-  modalidad:String;
- 
+  infoFiesta: String;
+  infoCanasta: String;
+  infoBaquitav1: String;
+  infoBaquitav2: String;
+  modalidad: String;
 
-invitar(){
-  //los forms en angular ahora son reactivos, asi que tuve que hacer un workarround para manipular la data
-  let invitado = this.emailForm.value.emailCtrl; //agarra el dato
-  this.invitados.push(invitado);  //agrega a una lista
-  
-  this.emailForm.get('emailCtrl').setValue(''); //lo blanquea
-  this.emailForm.get('emailCtrl').markAsPristine(); //limpiamos las validaciones
-  this.emailForm.get('emailCtrl').markAsUntouched(); // limpiamos las validaciones
-}
 
-  elegirModalidad(mod:String){
-    this.modalidad=mod;  
+  invitar() {
+    //los forms en angular ahora son reactivos, asi que tuve que hacer un workarround para manipular la data
+    let invitado = this.emailForm.value.emailCtrl; //agarra el dato
+    this.invitados.push(invitado);  //agrega a una lista
+
+    this.emailForm.get('emailCtrl').setValue(''); //lo blanquea
+    this.emailForm.get('emailCtrl').markAsPristine(); //limpiamos las validaciones
+    this.emailForm.get('emailCtrl').markAsUntouched(); // limpiamos las validaciones
   }
 
-  removerInvitado(inv){
-    // recorre lista de invitados, compara y lo saca de la lista
-    alert("implementar remover usuario")
+  elegirModalidad(mod: String) {
+    this.modalidad = mod;
   }
 
+  removerInvitado(inv) {
+    const index = this.invitados.indexOf(inv, 0);
+    if (index > -1) {
+      this.invitados.splice(index, 1);
+    }
+  }
 
-  cancelarCreacion(stepper){
+  cancelarCreacion(stepper) {
     this.invitados = [];
 
     stepper.reset();
     this.modalidad = null;
   }
+
+  //agregar validacion sobre mails que ya existen?
+  emailInvalido() {
+    return this.emailForm.get('emailCtrl').errors || this.emailForm.get('emailCtrl').pristine
+  }
+
 }
