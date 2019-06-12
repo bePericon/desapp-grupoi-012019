c32249c488aa00e2bfefd6b34a3c88cd9dbd298d
ale rossi <ale.h90@gmail.com>
2019-06-12 15:06:21 -0300

    primer version de agregar item

--- desapp-grupoi-frontend/src/app/agregar-items/agregar-items.component.ts
+++ desapp-grupoi-frontend/src/app/agregar-items/agregar-items.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-agregar-items',
@@ -7,9 +8,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarItemsComponent implements OnInit {
 
-  constructor() { }
+  items:any
+
+  nombreItem : string
+  personasPorUnidad: number
+  monto: number
+  cantidadItems: number
+
+  itemsParaElEvento: any
+
+  constructor() {
+
+    this.items  = [
+      {value: 'item1', viewValue: 'coca', rinde:2},
+      {value: 'item2', viewValue: 'lala', rinde:4},
+      {value: 'item3', viewValue: 'sarasa', rinde:3},
+      {value: 'item4', viewValue: 'ema', rinde:1},
+    ];
+
+    this.itemsParaElEvento =[]
+   }
 
   ngOnInit() {
   }
+
+
+  agregarAlListado(){
+    //   llamar al servicio, pasarle item
+  }
+
+  agregarAlEvento(){
+    //construye item con los datos del form
+    // y lo agrega a la lista
+    this.itemsParaElEvento.push()
+
+  }
+
+  agAlListadoEsInvalido(){
+    //   que esten los 2 datos cargados
+  }
+  agAlEventoEsInvalido(){
+
+    // que esten los 3 datos cargados
+    // return this.nombreItem && this.personasPorUnidad && this.monto
+  }
+
 
 }
