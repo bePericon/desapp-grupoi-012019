5074dc4d5e65d8171eb30667ad73d9117c6b3643
ale rossi <ale.h90@gmail.com>
2019-06-29 16:28:11 -0300

    sigo en mi intento de hacer un aspecto

--- desapp-grupoi-backend/src/main/java/app/aspect/MailSenderAspect.java
+++ desapp-grupoi-backend/src/main/java/app/aspect/MailSenderAspect.java
@@ -1,48 +1,41 @@
 package app.aspect;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.context.annotation.Configuration;
 
+
 @Aspect
 @Configuration
 public class MailSenderAspect {
 
-
-    @Before("execution(*app.service.account.*.*(..))")
-    public void before(JoinPoint joinPoint) {
-
-
-    	System.out.println("FUNCIONAAAAAAAAAAAAAAAAAAAAAAAAAAAAA1");
-    	System.out.print("sarasa");
-    }
-    
-    @After("execution(* app.service.account.*.*(..))")
-    public void afterReturning(JoinPoint joinPoint) {
-
-    	System.out.print("sarasa");
-    	System.out.println("FUNCIONAAAAAAAAAAAAAAAAAAAAAAAAAAAAA2");
-    }
+	private static final Logger logger = LogManager.getLogger(MailSenderAspect.class);
 	
+
+	@Before("execution(* app.service.event.*.*(..))")
+	public void beforee(JoinPoint joinPoint) {
+		// Advice
+		logger.info(" AAAAAAAAAAAAAAAAAAAAAAAAA ");
+		logger.info(" AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA3", joinPoint);
+	}
 	
-    
-    @Before("execution(*app.service.account.UsuarioService.*.*(..))")
-    public void before2(JoinPoint joinPoint) {
-        //Advice
-//        logger.info(" Check for user access ");
-//        logger.info(" Allowed execution for {}", joinPoint);
-    	System.out.print("sarasa");
-    	System.out.println("FUNCIONAAAAAAAAAAAAAAAAAAAAAAAAAAAAA3");
-    }
-    
-    @Before("execution(*app.service.account.UsuarioService.*.*(..))")
-    public void before3(JoinPoint joinPoint) {
-        //Advice
-//        logger.info(" Check for user access ");
-//        logger.info(" Allowed execution for {}", joinPoint);
-    	System.out.print("sarasa");
-    	System.out.println("FUNCIONAAAAAAAAAAAAAAAAAAAAAAAAAAAAA4");
-    }
+
+	@Before("execution(* app.service.event.*(..))")
+	public void beforeee(JoinPoint joinPoint) {
+		// Advice
+		logger.info(" AAAAAAAAAAAAAAAAAAAAAAAAA 2");
+		logger.info(" AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", joinPoint);
+	}
+	
+	@Before("execution(* app.service.event.*.(..))")
+	public void beforeee3(JoinPoint joinPoint) {
+		// Advice
+		logger.info(" AAAAAAAAAAAAAAAAAAAAAAAAA 1");
+		logger.info(" AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", joinPoint);
+	}
+
 }
